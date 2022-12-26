68c0347d3d6954928b141e52e70373d13345d8af
53CR3T <vineelvarma7@gmail.com>
2022-12-25 19:03:50 -0500

    thappu code

--- core_java/src/main/java/com/careerit/cj/basics/Grade.java
+++ core_java/src/main/java/com/careerit/cj/basics/Grade.java
@@ -7,17 +7,15 @@ public class Grade {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter marks");
         int score= sc.nextInt();
-        if (score >=0 && score<40){
-            System.out.println("D");
-        } else if (score>=40 && score<60 ) {
-            System.out.println("C");
-        } else if (score>=60 && score<80) {
-            System.out.println("B");
-
-        } else if (score>=80 && score<=100) {
-            System.out.println("A");
-        }else{
-            System.out.println("enter valid score");
+        switch(score) {
+            case 30:
+                System.out.println("fail");
+                break;
+            case 20:
+                System.out.println("pass");
+                break;
+            default:
+                System.out.println("code sarigga ledhu");
         }
 
         }
